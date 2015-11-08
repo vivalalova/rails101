f1badcf1e0b0065bf260e26cd789ae52324aa0f8
lovafuntek <lova@funtek.co>
2015-11-09 02:29:41 +0800

    scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
 	validates :content, presence: true
 
 	belongs_to :author, class_name: "User", foreign_key: :user_id
- 
+ 	
+ 	scope :recent, -> { order("updated_at DESC") }
+ 	
  	def editable_by?(user)
 	   	user && user == author
 	end
